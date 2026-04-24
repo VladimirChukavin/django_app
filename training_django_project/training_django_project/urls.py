ab75100451be1dafdab7b1bc5ee54b205bcf4051
master <chuk1976@mail.ru>
2026-04-25 01:00:11 +1000

    Добавил настройки для файлов.

--- training_django_project/training_django_project/urls.py
+++ training_django_project/training_django_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,3 +27,17 @@ urlpatterns = [
     path("req/", include("requestdataapp.urls")),
     path("accounts/", include("myauth.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        static(
+            settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT,
+        )
+    )
+    urlpatterns.extend(
+        static(
+            settings.STATIC_URL,
+            document_root=settings.STATIC_ROOT,
+        )
+    )
