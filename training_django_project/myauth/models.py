f1085cef7c5cab0e9a857dd921885562903f9b93
master <chuk1976@mail.ru>
2026-04-25 00:53:49 +1000

    Добавил поле для аватарки в профиле и функцию для установки пути загрузки файла.

--- training_django_project/myauth/models.py
+++ training_django_project/myauth/models.py
@@ -2,7 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+def profile_avatar_path(instance: "Profile", filename: str) -> str:
+    return "profiles/profile_{pk}/avatar/{filename}".format(
+        pk=instance.pk,
+        filename=filename,
+    )
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(max_length=500, blank=True)
     agreement_accepted = models.BooleanField(default=False)
+    avatar = models.ImageField(null=True, blank=True, upload_to=profile_avatar_path)
