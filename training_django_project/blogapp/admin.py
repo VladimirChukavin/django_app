b3dbeae3a5042fb484e2a035930904331fcb26d5
master <chuk1976@mail.ru>
2026-05-04 03:30:20 +1000

    Добавил настройки для админки blogapp.

--- training_django_project/blogapp/admin.py
+++ training_django_project/blogapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+@admin.register(Article)
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "content",
+        "pub_date",
+        "author__name",
+        "category__name",
+    )
+    list_display_links = ("id", "title")
+    ordering = ("id",)
+    search_fields = ("title", "content", "pub_date", "author__name", "category__name")
